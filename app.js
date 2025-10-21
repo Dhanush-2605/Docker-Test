d371b104d47bf34855e6d6c7a3365d603a3543b6
Dhanush <90703523+Dhanush-2605@users.noreply.github.com>
2025-10-21 16:36:12 +0530

    Implement file check and read message in API
    Added file existence check and message retrieval.

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const fs=require('fs')
 const app = express();
+
+
+const dateFilePath=path.join(__dirname,'data','data.txt')
 
 app.get('/api', (req, res) => {
   res.send('Hello from my DevOps project 👋');
+
+  try{
+    if (fs.existsSync(dateFilePath)) {
+      message=fs.readFileSync(dateFilePath,'utf8')
+    }
+  }
+  catch(err){
+    res.send('File does not exist!!')
+  
+  }
+
+    res.send(`<h2>📦 Message from Docker Volume:</h2><p>${message}</p>`);
+
 });
 
 const port = 3000;
